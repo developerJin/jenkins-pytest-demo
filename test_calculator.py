de4035ba96a3dc18635f8fff383a0c485b9d0785
developerJin <gpwlsdl153@gmail.com>
2025-11-03 19:20:07 +0900

    cal

---
+++ test_calculator.py
@@ -0,0 +1,26 @@
+import pytest
+from calculator import Calculator
+
+class TestCaculator:
+    def setup_method(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        assert self.calc.add(2, 3) == 5
+        assert self.calc.add(-1, 3) == 0
+
+    def test_sub(self):
+        assert self.calc.subtract(5, 3) == 2
+        assert self.calc.subtract(0, 5) == -5
+
+    def test_multiply(self):
+        assert self.calc.multiply(2, 3) == 6
+        assert self.calc.multiply(-1, 3) == -3
+
+    def test_divide(self):
+        assert self.calc.divide(10, 2) == 5
+        assert self.calc.divide(9, 3) == 3
+
+    # def test_divide(self):
+    #     assert self.calc.divide(10, 2) == 5
+    #     assert self.calc.divide(9, 3) == 3
