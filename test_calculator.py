be230e2ff9dca761cd03c7e39959497aad58ac84
developerJin <gpwlsdl153@gmail.com>
2025-11-03 20:21:27 +0900

    power add

--- test_calculator.py
+++ test_calculator.py
@@ -24,3 +24,7 @@ class TestCaculator:
     def test_divide_by_zero(self):
         with pytest.raises(ValueError):
             self.calc.divide(10, 0)
+
+    def test_power(self):
+        assert self.calc.power(2, 3) == 8
+        assert self.calc.power(5, 2) == 25
